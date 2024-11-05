890a8d59b670b89f044fd37b22e71e2f4e84734a
EdgarHTT <Edgarhumberto1999@gmail.com>
2024-11-05 12:14:16 -0600

    Connected frontend and backend

--- flask-server/server.py
+++ flask-server/server.py
@@ -1,10 +1,10 @@
-from flask import Flask, jsonify
+from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/members')
 def members():
-    return jsonify(["member1", "member2", "member3"])
+    return {"members": ["member1", "member2", "member3"]}
 
 if __name__ == '__main__':
     app.run(debug=True)
