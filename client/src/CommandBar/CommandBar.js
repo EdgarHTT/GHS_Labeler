ebe675fd8022ee3f47b41f5ca324c90562df2a9d
Edgar Tijerina <Edgarhumberto1999@gmail.com>
2024-11-06 23:07:35 -0600

    Added query and search bar functionality on the frontend

--- client/src/CommandBar/CommandBar.js
+++ client/src/CommandBar/CommandBar.js
@@ -1,12 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import SearchBar from './SearchBar/SearchBar'
 import GenerateButton from './GenerateButton/GenerateButton'
 
 function CommandBar() {
+    // State to store the search query
+    const [query, setQuery] = useState('');
+
+    // Handle the change in the search input
+    const handleSearchChange = (event) => {
+        setQuery(event.target.value);
+        // TODO: add the Auto-Complete API functionality
+    };
+
+    // Handle the submission of the form
+    const handleSubmit = () => {
+        alert(JSON.stringify({query}));
+        //TODO: I'll send the query to the backend perform the generation
+    }
     return (
         <div>
-            <SearchBar />
-            <GenerateButton />
+            {/* Passing state and handler to Searchbar and GenerateButton */}
+            <SearchBar query={query} onSearchChange={handleSearchChange} />
+            <GenerateButton onSubmit={handleSubmit} />
         </div>
     );
 }
--- client/src/CommandBar/SearchBar/SearchBar.js
+++ client/src/CommandBar/SearchBar/SearchBar.js
@@ -1,21 +1,28 @@
 import React from 'react'
 import Form from 'react-bootstrap/Form'
 
-function SearchBar() {
+function SearchBar({ query, onSearchChange }) {
     return (
     <>
-        <Form.Label htmlFor="searchQuery">Select Compound</Form.Label>
+        <Form.Label htmlFor="searchQuery">Search Compound</Form.Label>
         <Form.Control
             type="Query"
             id="searchQuery"
             aria-describedby="queryHelpBlock"
+            value={query}
+            onChange={onSearchChange}
         />
         <Form.Text id="queryHelpBlock" muted>
-            Search for a compound to generate it's label. You can use their
-            common name...
+            Search for a compound to generate it's label. You can use it's
+            common name.
         </Form.Text>
     </>
     );
 }
 
+/* 
+TODO: I plan to add the Auto-Complete Search Service from PubChem
+to give live suggestions as the user is typing the compound 
+in the search bar
+*/
 export default SearchBar;
