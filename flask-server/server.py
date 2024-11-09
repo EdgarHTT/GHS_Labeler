d7ed45128846da276a421abef52da6bbf3700ac7
Edgar Tijerina <Edgarhumberto1999@gmail.com>
2024-11-09 17:33:06 -0600

    Added GHS api request to backend

--- flask-server/server.py
+++ flask-server/server.py
@@ -23,11 +23,32 @@ def generationRequest():
         
         #Extracting CID
         cid = response.json()
-        cid = cid['IdentifierList']['CID']
+        # Turn to string
+        cid = cid['IdentifierList']['CID'][0]
+        # Debug log
+        app.logger.debug('Compound CID: ', cid)
 
     else:
         print("Compound not found or an error occurred.", response.status_code)
         return response.status_code
+    
+    # Create appropiate REST api request for GHS Classification
+    api_url_GHS = f'https://pubchem.ncbi.nlm.nih.gov/rest/pug_view/data/compound/{cid}/JSON/?response_type=display&heading=GHS%20Classification'
+
+    # Make request
+    response_GHS = requests.get(api_url_GHS)
+
+    # Handle non successful response
+    if response_GHS.status_code != 200:
+        
+        print("Compound not found or an error occurred.", response_GHS.status_code)
+        return response_GHS.status_code
+    
+    app.logger.debug(response_GHS.json())
+    return response_GHS.json()
+    
+    
+
 
 if __name__ == '__main__':
     app.run(debug=True)
