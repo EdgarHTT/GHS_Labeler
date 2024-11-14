588376dc8649f3bc15128e86e30eb5a32be0fc85
EdgarHTT <Edgarhumberto1999@gmail.com>
2024-11-14 13:03:37 -0600

    Reformat function completed

--- flask-server/helpers.py
+++ flask-server/helpers.py
@@ -2,23 +2,32 @@
 
 def reformat(compoundContent):
     
-    print(compoundContent['Record'], "\n")
-    print("-"*70)
-    print(compoundContent['Record'].keys(), "\n")
-    print("-"*70)
+    compoundData = {}
+    compoundData['num'] = compoundContent['Record']['RecordNumber']
+    compoundData['name'] = compoundContent['Record']['RecordTitle']
     
     # Shorten the dict to facilitate address
     shortCmpCont = compoundContent['Record']['Section'][0]['Section'][0]['Section'][0]['Information']
     
     for cont in shortCmpCont:
-        if cont['Name'] == "Precautionary Statement Codes":
-            print(cont['ReferenceNumber'], cont['Value']['StringWithMarkup'][0]['String'])
-        elif cont['Name'] == "Pictograms(s)":
-            print(cont['ReferenceNumber'], cont['Value']['StringWithMarkup'][0]['String'])
-        elif cont['Name'] == "GHS Hazard Statements":
-            hazards = cont['Value']['StringWithMarkup']
-            for hazard in hazards:
-                print(cont['ReferenceNumber'], hazard['String'])
-        else: print(cont, "\n")
 
-    return print("Done")
+        # Dict conts indexed with touples since the data is dependent on the reference number
+        if cont['Name'] == "Precautionary Statement Codes":
+        
+            compoundData[('Pcodes',f"{cont['ReferenceNumber']}")] = cont['Value']['StringWithMarkup'][0]['String'].split(", ")
+        
+        elif cont['Name'] == "Pictogram(s)":
+
+            # Extracts only the name of the pictogram from the list of dicts
+            compoundData[('Pictograms',f"{cont['ReferenceNumber']}")] = [item['Extra'] for item in cont['Value']['StringWithMarkup'][0]['Markup']]
+        
+        elif cont['Name'] == "GHS Hazard Statements":
+
+            hazards = cont['Value']['StringWithMarkup']
+            compoundData[('Hstatements',f"{cont['ReferenceNumber']}")] = [item['String'] for item in hazards]
+
+        elif cont['Name'] == "Signal":
+
+            compoundData[('Signal',f"{cont['ReferenceNumber']}")] = cont['Value']['StringWithMarkup'][0]['Markup'][0]["Extra"]
+
+    return print(compoundData)
